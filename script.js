109f65184f651069a46ae1b0cc7ea29d1546e500
giorgikilosanidze <125272790+giorgikilosanidze@users.noreply.github.com>
2024-04-19 03:40:22 +0400

    full

---
+++ script.js
@@ -0,0 +1,56 @@
+const input = document.querySelector(".input");
+const btn = document.querySelectorAll(".btn");
+
+// function click() {
+//     for (let i = 0; i < btn.length; i++) {
+//         btn[i].addEventListener("click", (e) => {
+//             let inputField = e.target.value;
+//             if (inputField === "AC") {
+//                 input.value = "";
+//             } else if (inputField === "DE") {
+//                 input.value = input.value.slice(0, -1);
+//             } else if (inputField === "=") {
+//                 input.value = eval(input.value);
+//             } else {
+//                 input.value += inputField;
+//             };
+//         });
+//     };
+// };
+
+// click();
+
+
+// function click2() {
+//     btn.forEach((i) => {
+//         i.addEventListener("click", (e) => {
+//             let inputField = e.target.value;
+//             if (inputField === "AC") {
+//                 input.value = "";
+//             } else if (inputField === "DE") {
+//                 input.value = input.value.slice(0, -1);
+//             } else if (inputField === "=") {
+//                 input.value = eval(input.value);
+//             } else {
+//                 input.value += inputField;
+//             };
+//         });
+//     });
+// };
+// click2();
+
+function click3(number) {
+    input.value += number;
+};
+
+function deleteAll() {
+    input.value = '';
+};
+
+function deleteOne() {
+    input.value = input.value.slice(0, -1);
+};
+
+function evaluation() {
+    input.value = eval(input.value);
+};
